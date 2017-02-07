26b7f079214db01cf5b4821f6d741cbf99a1747c
Yutaka Katayama <yutaka2487@gmail.com>
2017-02-07 23:21:08 +0900

    replace element with contents of spreadsheet

--- content.js
+++ content.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
+
+    var url = "https://docs.google.com/feeds/download/spreadsheets/Export"
+
     $(".spreadsheet").each(function(){
-        console.log(this.id);
-    })
+        var document_id = this.id;
+
+        var data = {
+            "key" : document_id,
+            "exportFormat" : "csv",
+            "gid" : 0
+        };
+
+        var callback = function(data) {
+            var table = $("<table>").append("<tbody>");
+            
+            data.split("\n").forEach(function(row){
+
+                var tr = $("<tr>").appendTo(table);
+                row.split(",").forEach(function(cell){
+                    $("<td>").appendTo(tr).text(cell);
+                });
+            
+            });
+
+            $("#"+document_id).replaceWith(table)
+        };
+
+        $.get(url, data, callback);
+    });
 });
